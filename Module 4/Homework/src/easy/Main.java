3dc1f6e0cc4de49e14e5a32f88611e1f1272302c
Anisa <supercyplata@gmail.com>
2025-02-20 00:00:23 +0500

    Я устал, вы тоже?

--- Module 4/Homework/src/easy/Main.java
+++ Module 4/Homework/src/easy/Main.java
@@ -3,7 +3,17 @@ package easy;
 public class Main {
 
   public static void main(String[] args) {
-    
+    StringPrinter printer = new StringPrinter();
+    printer.print("Hello, world!");
   }
 
+}
+interface Printer<T> {
+  void print(T value);
+}
+class StringPrinter implements Printer<String> {
+  @Override
+  public void print(String value) {
+    System.out.println(value);
+  }
 }
