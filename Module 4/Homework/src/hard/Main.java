cbb7fdbb38d362c87999bc0ccb51a11ea5d4ccd6
Anisa <supercyplata@gmail.com>
2025-02-20 20:40:54 +0500

    При проверке через intelliJ IDEA ошибка в UserData, почему...

--- Module 4/Homework/src/hard/Main.java
+++ Module 4/Homework/src/hard/Main.java
@@ -1,9 +1,39 @@
 package hard;
 
-public class Main {
-
-  public static void main(String[] args) {
-
+class Box<T> {
+  private T item;
+  public Box(T item) {
+    this.item = item;
+  }
+  public void setItem(T item) {
+    this.item = item;
   }
 
+  public T getItem() {
+    return item;
+  }
+
+  public void showType() {
+    System.out.println("Тип объекта: " + item.getClass().getName());
+  }
+
+  public static void main(String[] args) {
+    Box<String> stringBox = new Box<>("Привет, мир!");
+    System.out.println("Содержимое: " + stringBox.getItem());
+    stringBox.showType();
+    class MyData {
+      private int value;
+      public MyData(int value) {
+        this.value = value;
+      }
+      @Override
+      public String toString() {
+        return "MyData: " + value;
+      }
+    }
+
+    Box<MyData> dataBox = new Box<>(new MyData(42));
+    System.out.println("Содержимое: " + dataBox.getItem());
+    dataBox.showType();
+  }
 }
